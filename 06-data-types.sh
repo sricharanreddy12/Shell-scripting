2478ae99addf4558aef2a8f309daa16dfeb4c097
Sri Charan Reddy <charankudumurula@gmail.com>
2025-11-17 17:01:19 +0530

    variables

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -4,8 +4,8 @@ Number1=500
 Number2=659
 
 Timestamp=$(date)
-echo"script executed at: $Timestamp"
+echo "script executed at: $Timestamp"
 
-sum=$(($Number1+$Number2))
+Sum=$(($Number1+$Number2))
 
-echo "sum of $Number1 and $Number2 :$sum
+echo "sum of $Number1 and $Number2 :$Sum"

a18254cd3fe78f5e4000e12bd4dabb7a3e620784
Sri Charan Reddy <charankudumurula@gmail.com>
2025-11-17 16:42:22 +0530

    variables

---
+++ 04-variable-arg.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+Developer=$1
+Developer2=$2
+
+echo "$Developer:: Hey $Developer2, How are You ?"
+echo "$Developer2:: Hello, I am doing well. How about you ?"
+echo "$Developer :: I am doing good!!, what about you sprints ? "
+echo "$Developer2 :: currently, I am working on sprint. what about you?"
+echo "$Developer :: yup, same working on sprint for developing code" 

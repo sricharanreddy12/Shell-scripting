e0dd9e96553e6e62cf41ab4e4115c9fcc97e012f
Sri Charan Reddy <charankudumurula@gmail.com>
2025-11-17 16:27:33 +0530

    conversation

---
+++ 02-conversations.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+echo "Backend:: Hey Frontend, How are You ?"
+echo "Frontend:: Hello, I am doing well. How about you ?"
+echo "Backend :: I am doing good!!, what about you sprints ? "
+echo "Frontend :: currently, I am working on sprint. what about you?"
+echo "backend :: yup, same working on sprint for devloping code" 

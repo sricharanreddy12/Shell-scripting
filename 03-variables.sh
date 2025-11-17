145183a64fb13ef31bea036e786e4ccd902be73b
Sri Charan Reddy <charankudumurula@gmail.com>
2025-11-17 16:38:19 +0530

    variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,8 +3,8 @@
 Developer=Backend
 Developer2=Frontend
 
-echo "Developer:: Hey Developer2, How are You ?"
-echo "Developer2:: Hello, I am doing well. How about you ?"
-echo "Developer :: I am doing good!!, what about you sprints ? "
-echo "Developer2 :: currently, I am working on sprint. what about you?"
-echo "Developer :: yup, same working on sprint for developing code" 
+echo "$Developer:: Hey $Developer2, How are You ?"
+echo "$Developer2:: Hello, I am doing well. How about you ?"
+echo "$Developer :: I am doing good!!, what about you sprints ? "
+echo "$Developer2 :: currently, I am working on sprint. what about you?"
+echo "$Developer :: yup, same working on sprint for developing code" 

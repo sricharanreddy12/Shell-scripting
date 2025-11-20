2ec4c633b3487e73aba35099973677533bcdcb31
Sri Charan Reddy <charankudumurula@gmail.com>
2025-11-20 14:52:24 +0530

    specialvariables

---
+++ 09-conditions.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+Number=$1
+
+#-- -gt ---greater than
+#-- -lt --lessthan
+#-- -eq -- equals to
+#-- -ne -- not equals to
+
+if ($Number -gt 10)
+then
+   echo "given number $Number is greater than 10"
+else
+   echo " given number $Number is lessthan 10"
+fi      

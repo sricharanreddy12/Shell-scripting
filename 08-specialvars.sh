ef251ef2867d5b5fe0d61a5603f4a4167f8d9ea5
Sri Charan Reddy <charankudumurula@gmail.com>
2025-11-20 14:39:56 +0530

    specialvariables

---
+++ 08-specialvars.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+echo "Listing of the variable : $1"
+echo "All variables should be passed to the script: $@"
+echo "No.of variables : $#"
+echo "Script name: $0"
+echo "present working directory: $PWD"
+echo "user running the script: $USER"
+echo "Home directory of user: $HOME"
+echo "exit status of the command: $?"
+echo "PID of the script: $$"
+sleep 10 &
+echo " PID of last command moving to background: $!"

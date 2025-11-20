34ddabe95e1533f597714358a81821ade09ac2a1
Sri Charan Reddy <charankudumurula@gmail.com>
2025-11-20 15:13:30 +0530

    specialvariables

--- 10-installing-commands.sh
+++ 10-installing-commands.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0]
+if [ $USERID -ne 0 ]
 then
    echo "error; Run this script with root access"
    exit 1 # 0ther than 0 are not success, so we need to give 1 to 127
@@ -12,7 +12,7 @@ fi
 
 dnf install Mysql -y
 
-if [$? -eq 0]
+if [ $? -eq 0 ]
 then
     echo "Installing Mysql is success"
 else

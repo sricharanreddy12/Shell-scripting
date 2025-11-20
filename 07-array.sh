ef572523fa8641b7d1249d52f5916dcc1ca09e6c
Sri Charan Reddy <charankudumurula@gmail.com>
2025-11-20 14:15:51 +0530

    Arrays

--- 07-array.sh
+++ 07-array.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+# echo-commmand for printing
+
 Movies=("Spiderman" "Coolie" "Legend" "Dragon")
 
 echo "first Movie;${Movies[0]}"
+echo "Second Movie;${Movies[1]}"
+echo "List all Movies;${Movies[@]}"
